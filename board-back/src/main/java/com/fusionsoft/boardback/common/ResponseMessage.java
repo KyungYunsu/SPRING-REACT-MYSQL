215904816620bee103ec6b7c4f7e4b2144c0ca53
KyungYunsu <kchars82@gmail.com>
2024-03-11 19:16:39 +0900

    first commit

---
+++ board-back/src/main/java/com/fusionsoft/boardback/common/ResponseMessage.java
@@ -0,0 +1,22 @@
+package com.fusionsoft.boardback.common;
+
+public interface ResponseMessage {
+
+    // HTTP STATUS 200
+    String SUCCESS = "Success.";
+    // HTTP STATUS 400
+    String VALIDATION_FAILED = "Validation failed.";
+    String DUPLICATE_EMAIL = "Duplicate email.";
+    String DUPLICATE_NICKNAME = "Duplicate nickname.";
+    String DUPLICATE_TEL_NUMBER = "Duplicate tel number.";
+    String NOT_EXISTED_USER = "This user dose not exist";
+    String NOT_EXISTED_BOARD = "This board does not exist";
+    // HTTP STATUS 401
+    String SIGN_IN_FAIL = "Login information mismatch.";
+    String AUTHORIZATION_FAIL = "Authorization Failed.";
+    // HTTP STATUS 404
+    String NO_PERMISSION = "Do not have permission.";
+    // HTTP STATUS 500
+    String DATABASE_ERROR = "Database error.";
+    
+}
---
+++ board-front/src/App.tsx
@@ -0,0 +1,13 @@
+import Footer from 'layouts/Footer';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <Footer />      
+    </>
+  );
+}
+
+export default App;
