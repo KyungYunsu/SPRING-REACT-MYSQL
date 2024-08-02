5ccacab0093dc509fef18396e558b33ada1fa6fe
KyungYunsu <kys2021@fusionsoft.co.kr>
2024-08-02 12:36:52 +0900

    28. Auth 페이지 로그인 카드 B 완료

--- board-front/src/apis/index.ts
+++ board-front/src/apis/index.ts
@@ -1,1 +1,17 @@
-export const tmp = "";
+import SignInRequestDto from "./request/auth/sign-in.request.dto";
+import SignUpRequestDto from "./request/auth/sign-up.request.dto";
+
+const DOMAIN = 'http://localhost:4000';
+
+const API_DOMAIN = `${DOMAIN}/api/v1`;
+
+const SIGN_IN_URL = () => `${API_DOMAIN}/auth/sign_in`;
+const SIGN_UP_URL = () => `${API_DOMAIN}/auth/sign_up`;
+
+export const signInRequest = (requestBody: SignInRequestDto) => {
+
+}
+
+export const signUpRequest = (requestBody: SignUpRequestDto) => {
+    
+}
