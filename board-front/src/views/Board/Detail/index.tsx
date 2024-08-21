fd8691efe7093cae53f04255e6e76f7a93a72f11
KyungYunsu <kchars82@gmail.com>
2024-08-21 20:59:30 +0900

    48. 게시물 상세보기 상단 컴포넌트 구조 작성 완료

--- board-front/src/views/Board/Detail/index.tsx
+++ board-front/src/views/Board/Detail/index.tsx
@@ -1,11 +1,58 @@
-import React from 'react'
-import './style.css'
+import React from 'react';
+import './style.css';
 
-//       component: 게시물 상세 화면 컴포넌트       //
+//        component: 게시물 상세 화면 컴포넌트        //
 export default function BoardDetail() {
   
-  //       render: 게시물 상세 화면 컴포넌트 렌더링       //
+  //        component: 게시물 상세 상단 컴포넌트        //
+  const BoardDetailTop = () => {
+
+    //        render: 게시물 상세 상단 컴포넌트 렌더링        //
+    return (
+      <div id='board-detail-top'>
+        <div className='board-detail-top-header'>
+          <div className='board-detail-title'>{'오늘 점심 뭐먹지 맛있는 거 먹고 싶은데 추천 부탁 오늘 점심 뭐먹지 맛있는 거 먹고 싶은데'}</div>
+          <div className='board-detail-top-sub-box'>
+            <div className='board-detail-write-info-box'>
+              <div className='board-detail-sriter-profile-image'></div>
+              <div className='board-detail-writer-nickname'>{'안녕하세요나는주코야끼'}</div>
+              <div className='board-detail-info-divider'>{'\|'}</div>
+              <div className='board-detail-write-date'>{'2022. 05. 12.'}</div>
+            </div>
+            <div className='icon-button'>
+              <div className='icon more-icon'></div>
+            </div>
+            <div className='board-detail-more-box'>
+              <div className='board-detail-update-button'>{'수정'}</div>
+              <div className='divider'></div>
+              <div className='board-detail-delete-button'>{'삭제'}</div>
+            </div>
+          </div>
+        </div>
+        <div className='divider'></div>
+        <div className='board-detail-top-main'>
+          <div className='board-detail-main-text'>{'오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 나 점심때 오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 오늘 점심을 뭐먹을 지 너무 고민이 되는 오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 나 점심때 ...'}</div>
+          <div className='board-detail-main-image'></div>
+        </div>
+      </div>
+    )
+  }
+
+  //        component: 게시물 상세 하단 컴포넌트        //
+  const BoardDetailBottom = () => {
+
+    //        render: 게시물 상세 하단 컴포넌트 렌더링        //
+    return (<></>)
+  }
+
+
+  //        render: 게시물 상세 화면 컴포넌트 렌더링        //
   return (
-    <div>게시물 상세</div>
+    <div id='board-detail-wrapper'>
+      <div className='board-detail-container'>
+        <BoardDetailTop />
+        <BoardDetailBottom />
+      </div>
+    </div>
   )
 }
