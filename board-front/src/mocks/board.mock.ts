261468c834d19a5736b0b3b0c825de584991165c
KyungYunsu <kchars82@gmail.com>
2024-08-24 18:05:07 +0900

    52. 게시물 상세 페이지 이벤트 처리 완료

---
+++ board-front/src/mocks/board.mock.ts
@@ -0,0 +1,16 @@
+import exp from "constants";
+import { Board } from "types/interface";
+
+const boardMock: Board = {
+    boardNumber: 1,
+    title: '오늘 점심 뭐먹지 맛있는 거 먹고 싶은데 추천 부탁 오늘 점심 뭐먹지 맛있는 거 먹고 싶은데',
+    content: '오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 나 점심때 오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 오늘 점심을 뭐먹을 지 너무 고민이 되는 오늘 점심을 뭐먹을 지 너무 고민이 되는 데 뭐 먹을까? 나 점심때 ...',
+    boardImageList: ['https://png.pngtree.com/thumb_back/fh260/background/20230609/pngtree-three-puppies-with-their-mouths-open-are-posing-for-a-photo-image_2902292.jpg', 'https://image.bugsm.co.kr/album/images/500/40271/4027185.jpg'],
+    writeDatetime: '2022. 05. 12.',
+    writerEmail: 'email@email.com',
+    writerNickname: '안녕하세요나는주코야끼',
+    writerProfileImage: 'https://search.pstatic.net/common?type=b&size=3000&quality=100&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F202304%2F20230428214640258.jpg'
+
+}
+
+export default boardMock;
--- board-front/src/mocks/index.ts
+++ board-front/src/mocks/index.ts
@@ -2,10 +2,12 @@ import commentListMock from './comment-list.mock';
 import favoriteListMock from './favorite-list.mock';
 import top3BoardListMock from './top-3-board-list.mock';
 import latestBoardListMock from './latest-board-list.mock';
+import boardMock from './board.mock';
 
 export {
     commentListMock,
     favoriteListMock,
     top3BoardListMock,
-    latestBoardListMock
+    latestBoardListMock,
+    boardMock
 }
