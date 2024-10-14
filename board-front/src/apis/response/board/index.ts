0a274dd8aef70f94963190d7539be74c13159af5
KyungYunsu <kchars82@gmail.com>
2024-10-14 20:44:20 +0900

    73. 검색 페이지 API 연동 완료

--- board-front/src/apis/response/board/index.ts
+++ board-front/src/apis/response/board/index.ts
@@ -3,6 +3,7 @@ import GetBoardResponseDto from './get-board.response.dto';
 import GetCommentListResponseDto from './get-comment-list.response.dto';
 import GetFavoriteListResponseDto from './get-favorite-list.response.dto';
 import GetLatestBoardListResponseDto from './get-latest-board-list.response.dto';
+import GetSearchBoardListResponseDto from './get-search-board-list.response.dto';
 import GetTop3BoardListResponseDto from './get-top-3-board-list.response.dto';
 import IncreaseViewCountResponseDto from './increase-view-count.response.dto';
 import PatchBoardResponseDto from './patch-board-response.dto';
@@ -16,6 +17,7 @@ export type {
     GetCommentListResponseDto,
     GetFavoriteListResponseDto,
     GetLatestBoardListResponseDto,
+    GetSearchBoardListResponseDto,
     GetTop3BoardListResponseDto,
     IncreaseViewCountResponseDto,
     PatchBoardResponseDto,
--- board-front/src/apis/response/search/index.ts
+++ board-front/src/apis/response/search/index.ts
@@ -1,5 +1,8 @@
 import GetPopularListResponseDto from './get-popular-list.response.dto';
+import GetRelationListResponseDto from './get-relation-list.response.dto';
 
 export type {
-    GetPopularListResponseDto
+    GetPopularListResponseDto,
+    GetRelationListResponseDto
 };
+
